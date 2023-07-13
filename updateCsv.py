d1fc95ad1600b2ae8cbae8bb68ad0ccfdfaad2fe
abdochaoubii <abdochaoubi2000@gmail.com>
2023-07-13 16:10:42 +0100

    clean code

--- updateCsv.py
+++ updateCsv.py
@@ -1,8 +1,10 @@
 import json
 import csv
+
 import os
 from datetime import datetime
 import json
+
 
 def update_csv_with_json(csv_file, additional_data):
     existing_data = []
@@ -10,17 +12,16 @@ def update_csv_with_json(csv_file, additional_data):
         with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
             existing_data = list(reader)
-
-    
-    if type(additional_data) ==list:
+    if type(additional_data) == list:
         for row in additional_data:
             row['time_created'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         updated_data = existing_data + additional_data
-    elif type(additional_data) ==dict:
-        additional_data['time_created'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    elif type(additional_data) == dict:
+        additional_data['time_created'] = datetime.now().strftime(
+            '%Y-%m-%d %H:%M:%S')
         updated_data = existing_data
         updated_data.append(additional_data)
-    
+
     unique_keys = []
     for key_set in updated_data:
         for key in key_set.keys():
@@ -29,21 +30,19 @@ def update_csv_with_json(csv_file, additional_data):
     new_data = []
     new_unique_keys = []
     for key in updated_data:
-        spic_data={}
+        spic_data = {}
         for v in key:
-            print(f"{v} {key[v]}")
-            if(type(key[v]) == dict):
+            if (type(key[v]) == dict):
                 for k, t in key[v].items():
                     new_key = f"{v}::{k}"
-                    spic_data[new_key]=t
+                    spic_data[new_key] = t
                     if new_key not in new_unique_keys:
                         new_unique_keys.append(new_key)
             else:
                 if v not in new_unique_keys:
                     new_unique_keys.append(v)
-                spic_data[v]=key[v]
+                spic_data[v] = key[v]
         new_data.append(spic_data)
-    print(new_data)
     headers = list(new_unique_keys)
     with open(csv_file, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=headers)
@@ -56,5 +55,5 @@ def update_csv_with_json(csv_file, additional_data):
 csv_file = 'data.csv'
 json_file = 'data.json'
 with open(json_file, 'r') as file:
-        loadJson = json.load(file)
+    loadJson = json.load(file)
 update_csv_with_json(csv_file, loadJson)
